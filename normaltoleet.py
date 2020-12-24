15d8d7309b7cde4996e1ec543cca6baf311dffb0
Levis Münzberg <74121909+Yuconium@users.noreply.github.com>
2020-12-24 16:21:45 +0100

    Update normaltoleet.py

--- normaltoleet.py
+++ normaltoleet.py
@@ -2,16 +2,18 @@ from tkinter import *
 
 class App:
     def __init__(self, window):
+        self.normallab = Label(window, text = "Normal")
         self.Eingabe = Entry(window, width = 30)
-        self.Eingabe.pack()
+        self.leetlab = Label(window, text = "Leetspeak")
         self.Ausgabe = Entry(window, width = 30)
-        self.Ausgabe.pack()
-        self.Changer = Button(window, text = "lets l33t!", command = lambda: self.l33tfunktion())
-        self.Changer.pack()
         
-
-
-   
+        self.Changer = Button(window, text = "lets l33t!", command = lambda: self.l33tfunktion())
+        
+        self.normallab.pack()
+        self.Eingabe.pack()
+        self.leetlab.pack()
+        self.Ausgabe.pack()
+        self.Changer.pack()
        
 
     
@@ -20,10 +22,11 @@ class App:
             self.Ausgabe.insert(INSERT, "Couldnt find any words")
         else:
             self.normal = self.Eingabe.get()
-            self.Ausgabe.insert(INSERT, self.normal.replace("e", "3").replace("k", " |<").replace("a", "4").replace("S", "5").replace("o", "0").replace("O", "0")
+            self.Ausgabe.insert(INSERT, self.normal.replace("e", "3").replace("k", " |<").replace("a", "4").replace("S", "5").replace("o", "0").replace("O", "0"))
 
 
 if __name__ == "__main__":
+
     window = Tk()
     window.geometry("300x300")
     window.title("L33tsp33k")
